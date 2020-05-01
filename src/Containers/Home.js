1bb851343fc7e7a1b30ae208b422b481424f2318
sornasundaram <sorna@twinsdigital.com>
2020-05-02 01:19:32 +0530

    menu added

---
+++ src/Containers/Home.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+const home = () => {
+    return (
+       <div>
+           <p>sorna</p>
+           {/* <Navigation></Navigation> */}
+       </div>
+    //   
+    );
+};
+
+export default home
---
+++ src/Containers/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+
+
+const { Header } = Layout;
+const navigation = () => (
+
+    <Header>
+            <Menu mode="horizontal">
+
+            </Menu>
+    </Header>
+);
+
+export default navigation;
